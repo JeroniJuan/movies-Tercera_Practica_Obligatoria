e10926e63cae32c7cee31c5fd9ed426886ba0f79
Jeroni Juan <jjuanh@esliceu.com>
2025-01-15 18:21:22 +0100

    commit1

--- src/main/java/com/esliceu/movies/models/Production_Country.java
+++ src/main/java/com/esliceu/movies/models/Production_Country.java
@@ -5,30 +5,37 @@ import jakarta.persistence.*;
 @Entity
 public class Production_Country {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
-    @JoinColumn(referencedColumnName = "movie_id")
-    private int movie_id;
+    @JoinColumn(name = "movie_id", referencedColumnName = "movie_id")
+    private Movie movie;
 
     @ManyToOne
-    @JoinColumn(referencedColumnName = "country_id")
-    private int country_id;
+    @JoinColumn(name = "country_id", referencedColumnName = "country_id")
+    private Country country;
 
-    public int getMovie_id() {
-        return movie_id;
+    public Long getId() {
+        return id;
     }
 
-    public void setMovie_id(int movie_id) {
-        this.movie_id = movie_id;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public int getCountry_id() {
-        return country_id;
+    public Movie getMovie() {
+        return movie;
     }
 
-    public void setCountry_id(int country_id) {
-        this.country_id = country_id;
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
     }
 }
